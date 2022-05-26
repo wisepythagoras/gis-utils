92b218737c971265660da9dce3baf64d14329abb
Diogenis <55108282+wisepythagoras@users.noreply.github.com>
2022-05-25 22:50:19 -0400

    Remove unused function

--- cmd/render/main.go
+++ cmd/render/main.go
@@ -1,103 +1,16 @@
 package main
 
 import (
-	"context"
 	"flag"
 	"fmt"
-	"io"
 	"os"
 	"path/filepath"
 	"strings"
 
-	"github.com/paulmach/osm"
-	"github.com/paulmach/osm/osmpbf"
 	"github.com/tdewolff/canvas"
 	"github.com/wisepythagoras/gis-utils/config"
 	"github.com/wisepythagoras/gis-utils/gis"
-	"github.com/wroge/wgs84"
 )
-
-func ReadPBF(f io.Reader) ([]*gis.RichWay, *gis.BBox) {
-	nodeMap := make(map[osm.NodeID]*osm.Node)
-	ways := make([]*gis.RichWay, 0)
-
-	minLat := float64(999.0)
-	minLon := float64(999.0)
-	maxLat := float64(-999.0)
-	maxLon := float64(-999.0)
-
-	scanner := osmpbf.New(context.Background(), f, 8)
-	scanner.FilterNode = func(n *osm.Node) bool { return true }
-
-	defer scanner.Close()
-	// scanner.Object()
-
-	for scanner.Scan() {
-		o := scanner.Object()
-		t := o.ObjectID().Type()
-
-		if t == "node" {
-			// Add all of the nodes to the node map so that it's easily referenced.
-			node := o.(*osm.Node)
-			nodeMap[node.ID] = node
-
-			if node.Lat < minLat {
-				minLat = node.Lat
-			} else if node.Lat > maxLat {
-				maxLat = node.Lat
-			}
-
-			if node.Lon < minLon {
-				minLon = node.Lon
-			} else if node.Lon > maxLon {
-				maxLon = node.Lon
-			}
-		} else if t == "way" {
-			way := o.(*osm.Way)
-			nodeIDs := make([]osm.NodeID, 0)
-			points := make([]gis.Point, 0)
-
-			for _, wn := range way.Nodes {
-				nodeIDs = append(nodeIDs, wn.ID)
-				node := nodeMap[wn.ID]
-
-				if node != nil {
-					x, y, _ := wgs84.LonLat().To(wgs84.WebMercator())(node.Lon, node.Lat, 0)
-
-					point := gis.Point{
-						Lat: node.Lat,
-						Lon: node.Lon,
-						X:   x,
-						Y:   y,
-					}
-
-					points = append(points, point)
-				}
-			}
-
-			newWay := &gis.RichWay{
-				Way:     way,
-				NodeIDs: nodeIDs,
-				Points:  points,
-			}
-
-			ways = append(ways, newWay)
-		}
-	}
-
-	scanErr := scanner.Err()
-
-	if scanErr != nil {
-		panic(scanErr)
-	}
-
-	bbox := &gis.BBox{
-		SW: gis.Point{Lat: minLat, Lon: minLon},
-		NE: gis.Point{Lat: maxLat, Lon: maxLon},
-	}
-
-	return ways, bbox
-}
 
 func main() {
 	shapefilePtr := flag.String("shapefile", "", "The path to the land shapefile")
